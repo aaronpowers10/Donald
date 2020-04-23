0937b160583caff185a473d6fbd21def1f04ce15
cpoweraa <cpoweraa@M5320067.Go.JohnsonControls.com>
2020-04-23 13:20:50 -0400

    allow choice of doe22 and do23

--- Donald/src/donald/doe2_runner/DOE2Run.java
+++ Donald/src/donald/doe2_runner/DOE2Run.java
@@ -18,7 +18,13 @@
 
 package donald.doe2_runner;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
 import otis.lexical.UpdateListener;
@@ -31,51 +37,167 @@ public class DOE2Run implements Runnable {
 	private ArrayList<DOE2SimCompleteListener> simulationCompleteListeners;
 	private BDLErrorListener errorListener;
 	private boolean debugMode;
+	private DOE2Version version;
 
-	public DOE2Run(String inpFileName, String location, boolean debugMode){
+	public DOE2Run(String inpFileName, String location,DOE2Version version, boolean debugMode) {
 		this.inpFileName = inpFileName;
 		this.location = location;
+		this.version = version;
 		updateListeners = new ArrayList<UpdateListener>();
 		simulationCompleteListeners = new ArrayList<DOE2SimCompleteListener>();
 		errorListener = new BDLErrorListener();
 		updateListeners.add(errorListener);
 		this.debugMode = debugMode;
-		
+		// this.debugMode = true;
 	}
 
-	public void addUpdateListener(UpdateListener updateListener){
+	public void addUpdateListener(UpdateListener updateListener) {
 		updateListeners.add(updateListener);
 	}
-	
-	public void addSimulationCompleteListener(DOE2SimCompleteListener simulationCompleteListener){
+
+	public void addSimulationCompleteListener(DOE2SimCompleteListener simulationCompleteListener) {
 		simulationCompleteListeners.add(simulationCompleteListener);
 	}
 
 	@Override
 	public void run() {
-		
+
 		try {
+			deleteDOE2Files();
+			copyFile("DOEBDL.EXE");
+			copyFile("DOESIM.EXE");
+			copyFile("BDLKEY.BIN");
+			copyFile("HDRFIL.BIN");
+			copyFile("BDLLIB.DAT");
+			copyFile("BDLDFT.DAT");
+			File inpTemp = new File("INPUT2.TMP");
+			File inp = new File(inpFileName + ".inp");
+			File loc = new File(location + ".bin");
+			File locTemp = new File("WEATHER.BIN");
+			Files.copy(inp.toPath(), inpTemp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			Files.copy(loc.toPath(), locTemp.toPath(), StandardCopyOption.REPLACE_EXISTING);
 			Runtime runtime = Runtime.getRuntime();
-			//Process doeProcess = runtime.exec("c:\\doe22\\doe22.bat exe48y " + "\""+inpFileName+"\"" + " " + location);
-			for(UpdateListener updateListener:updateListeners){
-				updateListener.update("Running Simulation: " + inpFileName.substring(inpFileName.lastIndexOf("\\") + 1, inpFileName.length()));
+			for (UpdateListener updateListener : updateListeners) {
+				updateListener.update("Running Simulation: "
+						+ inpFileName.substring(inpFileName.lastIndexOf("\\") + 1, inpFileName.length()));
 			}
-			Process doeProcess = runtime.exec("c:\\doe22\\doe22.bat exe48y " + "\""+ inpFileName + "\""+ " " + "\""+ location + "\"");
-			StreamInterceptor streamInterceptor = new StreamInterceptor(doeProcess.getInputStream(),new DOE2SimProgressUpdater(updateListeners,debugMode));
+//			Process doeProcess = runtime.exec(System.getProperty("user.dir") + "\\src\\res\\DOEBDL", null,
+//					new File(System.getProperty("user.dir") + "\\src\\res"));
+			Process doeProcess = runtime.exec("DOEBDL");
+			StreamInterceptor streamInterceptor = new StreamInterceptor(doeProcess.getInputStream(),
+					new DOE2SimProgressUpdater(updateListeners, debugMode));
 			streamInterceptor.start();
 			doeProcess.waitFor();
-			for(DOE2SimCompleteListener simulationCompleteListener: simulationCompleteListeners){
-				simulationCompleteListener.simulationComplete(isError());
-			}
-			//System.out.println("FINISHED");
+//			doeProcess = runtime.exec(System.getProperty("user.dir") + "\\src\\res\\DOESIM", null,
+//					new File(System.getProperty("user.dir") + "\\src\\res"));
+			doeProcess = runtime.exec("DOESIM");
+			streamInterceptor = new StreamInterceptor(doeProcess.getInputStream(),
+					new DOE2SimProgressUpdater(updateListeners, debugMode));
+			streamInterceptor.start();
+			doeProcess.waitFor();
+			
+			File simFile = new File("DOESIM.OUT");
+			System.out.println(simFile.getAbsolutePath());
+			System.out.println(inpFileName);
+			simFile.renameTo(new File(inpFileName + ".SIM"));
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+
+		for (DOE2SimCompleteListener simulationCompleteListener : simulationCompleteListeners) {
+			simulationCompleteListener.simulationComplete(isError());
+		}
+		
+		deleteDOE2Files();
+
+		
+//		try {
+//			Runtime runtime = Runtime.getRuntime();
+//			//Process doeProcess = runtime.exec("c:\\doe22\\doe22.bat exe48y " + "\""+inpFileName+"\"" + " " + location);
+//			for(UpdateListener updateListener:updateListeners){
+//				updateListener.update("Running Simulation: " + inpFileName.substring(inpFileName.lastIndexOf("\\") + 1, inpFileName.length()));
+//			}
+//			Process doeProcess = runtime.exec("c:\\doe23\\doe23_ap.bat " + "\""+ inpFileName + "\""+ " " + "\""+ location + "\"");
+//			StreamInterceptor streamInterceptor = new StreamInterceptor(doeProcess.getInputStream(),new DOE2SimProgressUpdater(updateListeners,debugMode));
+//			streamInterceptor.start();
+//			doeProcess.waitFor();
+//			for(DOE2SimCompleteListener simulationCompleteListener: simulationCompleteListeners){
+//				simulationCompleteListener.simulationComplete(isError());
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
 	}
 	
-	public boolean isError(){
+	private void deleteDOE2Files() {
+		deleteFile("DOEBDL.EXE");
+		deleteFile("DOESIM.EXE");
+		deleteFile("BDLKEY.BIN");
+		deleteFile("HDRFIL.BIN");
+		deleteFile("BDLLIB.DAT");
+		deleteFile("BDLDFT.DAT");
+		deleteFile("CTRL.TMP");
+		deleteFile("DOEBDL.LOG");
+		deleteFile("DOESIM.LOG");
+		deleteFile("DOEHRREP.LIN");
+		deleteFile("DOEHRREP.SIN");
+		deleteFile("DOEREP.ERP");
+		deleteFile("DOEREP.SRP");
+		deleteFile("DOEREP.LRP");
+		deleteFile("PLTOUT.TMP");
+		deleteFile("STDFIL.TMP");
+		deleteFile("DSNFIL.TMP");
+		deleteFile("WEATHER.BIN");
+		deleteFile("INPUT2.TMP");
+		deleteFile("DOESIM.TXT");
+		deleteFile("plt.wth");
+		deleteFile("USR.LIB");
+	}
+
+	private void deleteFile(String fileName) {
+		File f = new File(fileName);
+		f.delete();
+	}
+
+	private void copyFile(String fileName) throws IOException {
+		InputStream input = null;
+		FileOutputStream output = null;
+//		try {
+		String resSubfolder = "";
+		if(version == DOE2Version.DOE2_2) {
+			resSubfolder = "22/";
+		} else if (version == DOE2Version.DOE2_3) {
+			resSubfolder = "23/";
+		}
+			URL u = this.getClass().getClassLoader().getResource("resources/" + resSubfolder +fileName);
+			input = u.openStream();
+			output = new FileOutputStream(fileName);
+
+			byte[] buffer = new byte[4096];
+			int bytesRead = input.read(buffer);
+			while (bytesRead != -1) {
+				output.write(buffer, 0, bytesRead);
+				bytesRead = input.read(buffer);
+			}
+			
+			input.close();
+			output.close();
+			//System.out.println("FINISHED COPYING " + fileName);
+//		} finally {
+//			//System.out.println("CLOSING " + fileName);
+//			input.close();
+//			output.close();
+//			System.out.println("CLOSING " + fileName);
+//
+//		}
+	}
+
+	public boolean isError() {
 		return errorListener.isError();
 	}
 }
---
+++ Donald/src/donald/doe2_runner/DOE2Version.java
@@ -0,0 +1,6 @@
+package donald.doe2_runner;
+
+public enum DOE2Version {
+	DOE2_2,DOE2_3
+
+}
